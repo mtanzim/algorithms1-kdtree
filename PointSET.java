52625302b30b82243fc1a9ac6b1409e93bf4545a
Tanzim Mokammel <mtanzim@gmail.com>
2019-05-01 15:52:51 +0200

    68%

--- PointSET.java
+++ PointSET.java
@@ -8,6 +8,7 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 public class PointSET {
 
@@ -68,7 +69,7 @@ public class PointSET {
 
     public Point2D nearest(Point2D p) {
         if (p == null) throw new IllegalArgumentException("Null p");
-        if (size() == 0) throw new IllegalArgumentException("Empty set!");
+        if (size() == 0) return null;
 
         class Node implements Comparable<Node> {
             private Point2D myCurP;
@@ -92,7 +93,7 @@ public class PointSET {
         }
         SET<Node> nearestPointSet = new SET<Node>();
         for (Point2D curPointInSet : pointSet) {
-            if (curPointInSet.equals(p)) continue;
+            // if (curPointInSet.equals(p)) continue;
             Node curNode = new Node(curPointInSet, curPointInSet.distanceSquaredTo(p));
             nearestPointSet.add(curNode);
         }
@@ -101,34 +102,58 @@ public class PointSET {
 
     public static void main(String[] args) {
         // StdOut.println("Hello!");
+        // PointSET ps = new PointSET();
+        // ps.insert(new Point2D(0.1, 0.2));
+        // ps.insert(new Point2D(0.2, 0.2));
+        // ps.insert(new Point2D(0.3, 0.2));
+        // ps.insert(new Point2D(0.4, 0.2));
+        // ps.insert(new Point2D(0.5, 0.2));
+        // ps.insert(new Point2D(0.9, 0.3));
+        // ps.insert(new Point2D(0.8, 0.4));
+        // ps.insert(new Point2D(0.95, 0.5));
+        // RectHV searchRect = new RectHV(0.10, 0.15, 0.55, 0.55);
+        // ps.range(searchRect);
+        // StdDraw.setPenRadius(0.01);
+        // StdDraw.setPenColor(StdDraw.RED);
+        // ps.draw();
+        // StdDraw.setPenRadius(0.01);
+        // StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+        // searchRect.draw();
+        // // StdOut.println(ps.toString());
+        // StdDraw.setPenColor(StdDraw.GREEN);
+        // for (Point2D insideP : ps.range(searchRect)) {
+        //     insideP.draw();
+        // }
+        // Point2D checkNeighborPoint = new Point2D(0.98, 0.5);
+        // StdDraw.setPenColor(StdDraw.BLACK);
+        // checkNeighborPoint.draw();
+        // Point2D nearestPoint = ps.nearest(checkNeighborPoint);
+        // StdDraw.setPenColor(StdDraw.MAGENTA);
+        // nearestPoint.draw();
+
+
+        // Failed test 3
         PointSET ps = new PointSET();
-        ps.insert(new Point2D(0.1, 0.2));
-        ps.insert(new Point2D(0.2, 0.2));
-        ps.insert(new Point2D(0.3, 0.2));
-        ps.insert(new Point2D(0.4, 0.2));
-        ps.insert(new Point2D(0.5, 0.2));
-        ps.insert(new Point2D(0.9, 0.3));
-        ps.insert(new Point2D(0.8, 0.4));
-        ps.insert(new Point2D(0.95, 0.5));
-        RectHV searchRect = new RectHV(0.10, 0.15, 0.55, 0.55);
-        ps.range(searchRect);
-        StdDraw.setPenRadius(0.01);
+        ps.insert(new Point2D(1.0, 0.75));
+        ps.insert(new Point2D(0.25, 0.5));
+        ps.insert(new Point2D(1.0, 0.75));
+        ps.insert(new Point2D(0.5, 0.75));
+        ps.insert(new Point2D(0.0, 0.25));
+        ps.insert(new Point2D(1.0, 0.5));
+        ps.insert(new Point2D(1.0, 0.5));
+        ps.insert(new Point2D(0.25, 0.25));
+        ps.insert(new Point2D(0.75, 0.25));
+        ps.insert(new Point2D(0.5, 0.75));
+        StdDraw.setPenRadius(0.02);
         StdDraw.setPenColor(StdDraw.RED);
         ps.draw();
-        StdDraw.setPenRadius(0.01);
-        StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
-        searchRect.draw();
-        // StdOut.println(ps.toString());
-        StdDraw.setPenColor(StdDraw.GREEN);
-        for (Point2D insideP : ps.range(searchRect)) {
-            insideP.draw();
-        }
-        Point2D checkNeighborPoint = new Point2D(0.98, 0.5);
-        StdDraw.setPenColor(StdDraw.BLACK);
-        checkNeighborPoint.draw();
-        Point2D nearestPoint = ps.nearest(checkNeighborPoint);
+        Point2D query = new Point2D(0.0, 0.25);
         StdDraw.setPenColor(StdDraw.MAGENTA);
-        nearestPoint.draw();
+        query.draw();
+        Point2D nearest = ps.nearest(query);
+        StdOut.println(nearest.toString());
+        StdDraw.setPenColor(StdDraw.GREEN);
+        nearest.draw();
 
 
     }
